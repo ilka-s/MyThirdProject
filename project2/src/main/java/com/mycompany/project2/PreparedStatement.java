d28359388ea3df7f34851da403e7b31b8907bbcd
ilka-s <83311067+ilka-s@users.noreply.github.com>
2021-06-14 23:22:19 +0200

    Add files via upload

---
+++ project2/src/main/java/com/mycompany/project2/PreparedStatement.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.project2;
+
+import java.sql.ResultSet;
+
+/**
+ *
+ * @author User
+ */
+public class PreparedStatement {
+
+    void setString(int i, String emri) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    int executeUpdate() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    ResultSet executeQuery() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
